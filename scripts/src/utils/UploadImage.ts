1320521cde306ae28ae3b550d82d84b6a411b718
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-23 17:31:46 +0530

    Script: Fix image upload mimetype

--- scripts/src/utils/UploadImage.ts
+++ scripts/src/utils/UploadImage.ts
@@ -5,17 +5,19 @@ import supabase from "../supabase.js";
  * @param bucketName: Name of the supabase bucket/folder to upload to
  * @param fileName: Filename that is saved in supabase
  * @param file: The file to be uploaded
- * @param upsert: Whether to update the existing file
+ * @param upsert: Whether to update the existing file. Default "true"
+ * @param contentType: Type of file. Default "image/png"
  */
 export async function uploadImage(
     bucketName: string,
     fileName: string,
     file: Buffer | ArrayBuffer | Uint8Array,
     upsert: boolean = true,
+    contentType: "image/png",
 ) {
     const { data, error } = await supabase.storage
         .from(bucketName)
-        .upload(fileName, file, { upsert: upsert });
+        .upload(fileName, file, { upsert: upsert, contentType: contentType });
 
     if (error) console.error(`Error uploading image: ${error.message}`);
     else return data;
