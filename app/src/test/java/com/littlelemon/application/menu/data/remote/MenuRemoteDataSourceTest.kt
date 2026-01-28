f27f9e28039c3dc6760460aa9bd584ced769f2ad
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-28 16:37:57 +0530

    Add MenuRemoteDataSourceTest

---
+++ app/src/test/java/com/littlelemon/application/menu/data/remote/MenuRemoteDataSourceTest.kt
@@ -0,0 +1,78 @@
+package com.littlelemon.application.menu.data.remote
+
+import com.littlelemon.application.menu.data.remote.models.DishDTO
+import com.littlelemon.application.menu.utils.generateDishDTO
+import com.littlelemon.application.utils.createFakeSupabaseClient
+import io.github.jan.supabase.exceptions.RestException
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class MenuRemoteDataSourceTest {
+
+    @Test
+    fun fetchDishes_remoteSuccessWithCorrectJson_returnsCorrectDTOObject() = runTest {
+        // Arrange
+        val dishes = List<DishDTO>(5) { generateDishDTO() }
+        val responseData = Json.encodeToString(dishes)
+        val client = createFakeSupabaseClient(requestHandler = { request ->
+            respond(
+                content = responseData,
+                status = HttpStatusCode.OK,
+                headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        })
+        val dataSource = MenuRemoteDataSource(client)
+
+        // Act
+        val result = dataSource.fetchDishes()
+
+        // Assert
+        assertEquals(dishes.size, result.size)
+        assertTrue(result.mapIndexed { index, dish -> dish == dishes[index] }
+            .all { matching -> matching })
+    }
+
+    @Test
+    fun fetchDishes_remoteSuccessWithEmptyBody_returnsEmptyList() = runTest {
+
+        // Arrange
+        val client = createFakeSupabaseClient(requestHandler = {
+
+            respond(
+                "[]", status = HttpStatusCode.OK,
+                headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        })
+        val dataSource = MenuRemoteDataSource(client)
+
+        // Act
+        val result = dataSource.fetchDishes()
+
+        // Assert
+        assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun fetchDishes_remoteError_returnsEmptyList() = runTest {
+
+        // Arrange
+        val client = createFakeSupabaseClient(requestHandler = {
+            respond(
+                """{"message": "Not Found", "code": "404"}""", status = HttpStatusCode.BadRequest,
+                headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        })
+        val dataSource = MenuRemoteDataSource(client)
+
+        // Act & Assert
+        assertThrows<RestException> { dataSource.fetchDishes() }
+    }
+}
---
+++ app/src/test/java/com/littlelemon/application/utils/FakeSupabaseClient.kt
@@ -0,0 +1,38 @@
+package com.littlelemon.application.utils
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.SupabaseClientBuilder
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.logging.LogLevel
+import io.github.jan.supabase.postgrest.Postgrest
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.MockRequestHandleScope
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.request.HttpRequestData
+import io.ktor.client.request.HttpResponseData
+
+// Taken from supabase-kt test-common
+// https://github.com/supabase-community/supabase-kt/blob/master/test-common/src/commonMain/kotlin/io/github/jan/supabase/testing/SupabaseClientMock.kt
+// TODO: Refactor to support plugins
+fun createFakeSupabaseClient(
+    supabaseURL: String = "https://supabase.com/",
+    supabaseKey: String = "fake-supabase-key",
+    configuration: SupabaseClientBuilder.() -> Unit = {},
+    requestHandler: suspend MockRequestHandleScope.(HttpRequestData) -> HttpResponseData = {
+        respond(
+            ""
+        )
+    },
+): SupabaseClient {
+    return createSupabaseClient(
+        supabaseUrl = supabaseURL,
+        supabaseKey = supabaseKey,
+    ) {
+        defaultLogLevel = LogLevel.NONE // For Unit Testing, Logging is disabled
+        install(Postgrest)
+        httpEngine = MockEngine { request ->
+            requestHandler(request)
+        }
+        configuration()
+    }
+}
