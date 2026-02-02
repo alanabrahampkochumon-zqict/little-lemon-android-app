c62ce42cb613746edef4cabcd1beb7c221894563
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-02 17:25:05 +0530

    Add AuthViewModelTest for email change

--- app/src/test/java/com/littlelemon/application/auth/presentation/AuthViewModelTest.kt
+++ app/src/test/java/com/littlelemon/application/auth/presentation/AuthViewModelTest.kt
@@ -1,6 +1,10 @@
 package com.littlelemon.application.auth.presentation
 
+import com.littlelemon.application.auth.presentation.components.AuthActions
+import com.littlelemon.application.utils.MainTestDispatcherRule
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -10,12 +14,21 @@ class AuthViewModelTest {
     @Nested
     inner class StateModificationTests {
 
-//        @Rule
-//        val mainDispatcherRule = MainDisp
+        @get:Rule
+        val mainDispatcherRule = MainTestDispatcherRule()
+
+        val viewModel = AuthViewModel()
 
         @Test
         fun onEmailChange_toValidEmail_stateUpdatesToNewEmail() = runTest {
+            // Arrange
+            val newText = "test@gmail.com"
 
+            // Act
+            viewModel.onAction(AuthActions.ChangeEmail(email = newText))
+
+            // Assert
+            assertEquals(newText, viewModel.state.value.email)
         }
     }
 }
