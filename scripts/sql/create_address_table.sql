bf95e2eec92ca82b241348c948845c44e2243b8f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-07 15:15:25 +0530

    Scripts: Add address table sql

---
+++ scripts/sql/create_address_table.sql
@@ -0,0 +1,35 @@
+-- NOTE: Enable postgis extension from the database.
+-- https://supabase.com/docs/guides/database/extensions/postgis
+CREATE TABLE
+    IF NOT EXISTS user_address (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        user_id UUID NOT NULL REFERENCES auth.users (id) ON DELETE CASCADE DEFAULT auth.uid (),
+        label TEXT,
+        address TEXT,
+        streetAddress TEXT,
+        city VARCHAR(50),
+        state VARCHAR(50),
+        pin_code VARCHAR(20),
+        location geography (POINT),
+        created_at TIMESTAMP DEFAULT now ()
+    );
+
+-- RLS to prevent users from creating more than 5 addresses
+ALTER TABLE user_address ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Limit addresses per user" ON user_address FOR INSERT TO authenticated
+WITH
+    CHECK (
+        (
+            SELECT
+                count(*)
+            FROM
+                user_address
+            WHERE
+                user_id = auth.uid ()
+        ) < 5
+    );
+
+CREATE POLICY "User can see their own addresses" ON user_address FOR
+SELECT
+    USING (auth.uid () = user_id);
