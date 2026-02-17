158ad891985638dd674dea39ae2ba76f648f0bda
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-17 17:25:01 +0530

    Add progress indicator for resend code

--- app/src/main/java/com/littlelemon/application/auth/presentation/VerificationScreen.kt
+++ app/src/main/java/com/littlelemon/application/auth/presentation/VerificationScreen.kt
@@ -1,7 +1,9 @@
 package com.littlelemon.application.auth.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,14 +11,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,6 +40,8 @@ import com.littlelemon.application.core.presentation.designsystem.components.But
 import com.littlelemon.application.core.presentation.designsystem.components.TopNavigationBar
 import com.littlelemon.application.core.presentation.designsystem.dimens
 import com.littlelemon.application.core.presentation.designsystem.typeStyle
+import com.littlelemon.application.core.presentation.designsystem.xLarge
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -87,8 +100,58 @@ fun EmailSubsection(emailAddress: String, modifier: Modifier = Modifier, onClick
 }
 
 @Composable
-fun ResendTimer(modifier: Modifier = Modifier) {
-    Text("59")
+fun ResendTimer(modifier: Modifier = Modifier, initialTime: Long = 60) {
+
+    var timeLeft by rememberSaveable {
+        mutableStateOf(initialTime)
+    }
+    val isTimerRunning = timeLeft > 0
+
+    LaunchedEffect(key1 = timeLeft, key2 = isTimerRunning) {
+        if (isTimerRunning) {
+            delay(1000L)
+            timeLeft--
+        }
+    }
+
+    if (isTimerRunning) {
+        Box(modifier = modifier, contentAlignment = Alignment.Center) {
+            CircularProgressIndicator(
+                progress = { timeLeft / initialTime.toFloat() },
+                modifier = Modifier.size(
+                    MaterialTheme.dimens.size3XL
+                ),
+                strokeWidth = MaterialTheme.dimens.sizeXS,
+                strokeCap = StrokeCap.Round,
+                trackColor = MaterialTheme.colors.action
+            )
+            Text(
+                timeLeft.toString(),
+                style = MaterialTheme.typeStyle.labelSmall,
+                color = MaterialTheme.colors.contentTertiary
+            )
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = MaterialTheme.dimens.sizeLG,
+                    bottom = MaterialTheme.dimens.sizeLG,
+                    start = MaterialTheme.dimens.size2XL,
+                    end = MaterialTheme.dimens.size3XL
+                )
+                .background(
+                    MaterialTheme.colors.secondary,
+                    shape = MaterialTheme.shapes.xLarge.copy(
+                        topEnd = CornerSize(0.dp),
+                        bottomEnd = CornerSize(0.dp)
+                    )
+                )
+        ) {
+            Text(stringResource(R.string.act_resend_otp))
+        }
+    }
+
 }
 
 
@@ -111,7 +174,7 @@ private fun OTPFields(errorMessage: String? = null) {
                 )
             }
         }
-        
+
         errorMessage?.let { message ->
             Text(
                 text = message,
