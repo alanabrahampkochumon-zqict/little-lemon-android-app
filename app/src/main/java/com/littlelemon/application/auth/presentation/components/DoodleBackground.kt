e9a2d08277267bc6bb3bd597fb9fa46f8432a0b5
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-17 15:20:02 +0530

    Refactor Login Screen Root

---
+++ app/src/main/java/com/littlelemon/application/auth/presentation/components/DoodleBackground.kt
@@ -0,0 +1,36 @@
+package com.littlelemon.application.auth.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.littlelemon.application.R
+import com.littlelemon.application.core.presentation.designsystem.colors
+
+@Composable
+fun DoodleBackground(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(MaterialTheme.colors.secondary)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.doodles),
+            contentDescription = stringResource(R.string.desc_logo),
+            Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.contentHighlight),
+            alpha = 0.24f
+        )
+    }
+}
