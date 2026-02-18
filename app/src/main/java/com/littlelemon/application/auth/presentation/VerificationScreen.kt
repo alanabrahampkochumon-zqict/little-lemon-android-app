ce8236e28ed6b2f14bf6337b4d10e83920964fe8
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-18 14:31:39 +0530

    Add Resend Timer Impl

--- app/src/main/java/com/littlelemon/application/auth/presentation/VerificationScreen.kt
+++ app/src/main/java/com/littlelemon/application/auth/presentation/VerificationScreen.kt
@@ -1,6 +1,5 @@
 package com.littlelemon.application.auth.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,37 +10,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.littlelemon.application.R
 import com.littlelemon.application.auth.presentation.components.OTPInputField
+import com.littlelemon.application.auth.presentation.components.ResendTimer
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
 import com.littlelemon.application.core.presentation.designsystem.colors
 import com.littlelemon.application.core.presentation.designsystem.components.Button
 import com.littlelemon.application.core.presentation.designsystem.components.TopNavigationBar
 import com.littlelemon.application.core.presentation.designsystem.dimens
 import com.littlelemon.application.core.presentation.designsystem.typeStyle
-import com.littlelemon.application.core.presentation.designsystem.xLarge
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,27 +41,37 @@ fun VerificationContent(
     onVerifyEmail: () -> Unit = {},
     onChangeEmail: () -> Unit = {}
 ) {
-    TopNavigationBar(
-        label = stringResource(R.string.nav_verify_email),
-        navigationIcon = R.drawable.ic_caretleft,
-        navigationIconDescription = stringResource(R.string.act_back),
-        modifier = Modifier.heightIn(max = 48.dp)
-    )
-    Spacer(Modifier.height(MaterialTheme.dimens.sizeXL))
-    Column(
-        modifier = Modifier.padding(
-            start = MaterialTheme.dimens.sizeXL,
-            end = MaterialTheme.dimens.sizeXL,
-            bottom = MaterialTheme.dimens.size2XL
-        ),
-        verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.sizeXL)
-    ) {
-        //    EmailSubsection(emailAddress = authState.email ?: "test@gmail.com")
-        EmailSubsection(emailAddress = "test@gmail.com") // TODO: Replace with VM
-        OTPFields()
-        ResendTimer()
-        Spacer(Modifier.height(MaterialTheme.dimens.size3XL))
+    Column(modifier = modifier) {
+        TopNavigationBar(
+            label = stringResource(R.string.nav_verify_email),
+            navigationIcon = R.drawable.ic_caretleft,
+            navigationIconDescription = stringResource(R.string.act_back),
+            modifier = Modifier.heightIn(max = 48.dp)
+        )
+        Spacer(Modifier.height(MaterialTheme.dimens.sizeXL))
+        Column(
+            modifier = Modifier.padding(
+                start = MaterialTheme.dimens.sizeXL,
+                end = MaterialTheme.dimens.sizeXL,
+                bottom = MaterialTheme.dimens.sizeXL
+            ),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.sizeXL)
+        ) {
+            //    EmailSubsection(emailAddress = authState.email ?: "test@gmail.com")
+            EmailSubsection(emailAddress = "test@gmail.com") // TODO: Replace with VM
+            OTPFields()
+        }
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
+            ResendTimer()
+        }
+        if(isScrollable) {
+            Spacer(Modifier.height(MaterialTheme.dimens.size3XL))
+        } else {
+            Spacer(Modifier.weight(1f))
+        }
+        Box(modifier = Modifier.padding(horizontal = MaterialTheme.dimens.sizeXL)) {
         Button(stringResource(R.string.act_verify), onClick = onVerifyEmail)
+        }
     }
 }
 
@@ -97,61 +96,6 @@ fun EmailSubsection(emailAddress: String, modifier: Modifier = Modifier, onClick
             )
         }
     }
-}
-
-@Composable
-fun ResendTimer(modifier: Modifier = Modifier, initialTime: Long = 60) {
-
-    var timeLeft by rememberSaveable {
-        mutableStateOf(initialTime)
-    }
-    val isTimerRunning = timeLeft > 0
-
-    LaunchedEffect(key1 = timeLeft, key2 = isTimerRunning) {
-        if (isTimerRunning) {
-            delay(1000L)
-            timeLeft--
-        }
-    }
-
-    if (isTimerRunning) {
-        Box(modifier = modifier, contentAlignment = Alignment.Center) {
-            CircularProgressIndicator(
-                progress = { timeLeft / initialTime.toFloat() },
-                modifier = Modifier.size(
-                    MaterialTheme.dimens.size3XL
-                ),
-                strokeWidth = MaterialTheme.dimens.sizeXS,
-                strokeCap = StrokeCap.Round,
-                trackColor = MaterialTheme.colors.action
-            )
-            Text(
-                timeLeft.toString(),
-                style = MaterialTheme.typeStyle.labelSmall,
-                color = MaterialTheme.colors.contentTertiary
-            )
-        }
-    } else {
-        Box(
-            modifier = Modifier
-                .padding(
-                    top = MaterialTheme.dimens.sizeLG,
-                    bottom = MaterialTheme.dimens.sizeLG,
-                    start = MaterialTheme.dimens.size2XL,
-                    end = MaterialTheme.dimens.size3XL
-                )
-                .background(
-                    MaterialTheme.colors.secondary,
-                    shape = MaterialTheme.shapes.xLarge.copy(
-                        topEnd = CornerSize(0.dp),
-                        bottomEnd = CornerSize(0.dp)
-                    )
-                )
-        ) {
-            Text(stringResource(R.string.act_resend_otp))
-        }
-    }
-
 }
 
 
---
+++ app/src/main/java/com/littlelemon/application/auth/presentation/components/ResendTimer.kt
@@ -0,0 +1,114 @@
+package com.littlelemon.application.auth.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.littlelemon.application.R
+import com.littlelemon.application.core.presentation.designsystem.colors
+import com.littlelemon.application.core.presentation.designsystem.dimens
+import com.littlelemon.application.core.presentation.designsystem.typeStyle
+import com.littlelemon.application.core.presentation.designsystem.xLarge
+import kotlinx.coroutines.delay
+
+@Composable
+fun ResendTimer(
+    modifier: Modifier = Modifier,
+    initialTime: Int = 60,
+    totalTime: Int = 60,
+    onChangeEmail: () -> Unit = {}
+) {
+
+    var timeLeft by rememberSaveable {
+        mutableIntStateOf(initialTime)
+    }
+    val isTimerRunning = timeLeft > 0
+
+    LaunchedEffect(key1 = timeLeft, key2 = isTimerRunning) {
+        if (isTimerRunning) {
+            delay(1000L)
+            timeLeft--
+        }
+    }
+
+    if (isTimerRunning) {
+        Box(modifier = Modifier.padding(end = MaterialTheme.dimens.size2XL)) {
+            Box(modifier = modifier, contentAlignment = Alignment.Center) {
+                CircularProgressIndicator(
+                    progress = { timeLeft / totalTime.toFloat() },
+                    modifier = Modifier.size(
+                        MaterialTheme.dimens.size3XL
+                    ),
+                    strokeWidth = MaterialTheme.dimens.sizeXS,
+                    strokeCap = StrokeCap.Round,
+                    trackColor = MaterialTheme.colors.action,
+                    color = MaterialTheme.colors.contentHighlight
+                )
+                Text(
+                    timeLeft.toString(),
+                    style = MaterialTheme.typeStyle.labelSmall,
+                    color = MaterialTheme.colors.contentTertiary
+                )
+            }
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .background(
+                    MaterialTheme.colors.action,
+                    shape = MaterialTheme.shapes.xLarge.copy(
+                        topEnd = CornerSize(0.dp),
+                        bottomEnd = CornerSize(0.dp)
+                    )
+                )
+                .padding(
+                    top = MaterialTheme.dimens.sizeLG,
+                    bottom = MaterialTheme.dimens.sizeLG,
+                    start = MaterialTheme.dimens.size2XL,
+                    end = MaterialTheme.dimens.size3XL
+                )
+                .clickable { onChangeEmail() }
+
+        ) {
+            Text(
+                stringResource(R.string.act_resend_otp),
+                style = MaterialTheme.typeStyle.labelMedium,
+                color = MaterialTheme.colors.contentHighlight
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ResendTimerPreview() {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.End
+    ) {
+        ResendTimer()
+        ResendTimer(initialTime = 30)
+        ResendTimer(initialTime = 0)
+    }
+}
