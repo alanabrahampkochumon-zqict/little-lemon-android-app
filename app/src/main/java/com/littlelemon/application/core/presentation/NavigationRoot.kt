6930f4118939fb8946d947a915c646f49289000c
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-07 15:26:22 +0530

    Update supabase docker file

--- app/src/main/java/com/littlelemon/application/core/presentation/NavigationRoot.kt
+++ app/src/main/java/com/littlelemon/application/core/presentation/NavigationRoot.kt
@@ -1,27 +1,18 @@
 package com.littlelemon.application.core.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalWindowInfo
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.littlelemon.application.address.presentation.AddressViewModel
-import com.littlelemon.application.address.presentation.screens.LocationEntryContent
 import com.littlelemon.application.address.presentation.screens.LocationPermissionScreen
 import com.littlelemon.application.auth.presentation.AuthViewModel
 import com.littlelemon.application.auth.presentation.screens.AuthScreen
 import com.littlelemon.application.core.domain.model.SessionStatus
-import com.littlelemon.application.core.presentation.components.BottomSheet
-import com.littlelemon.application.core.presentation.components.Button
 import com.littlelemon.application.home.presentation.HomeScreen
+import io.github.jan.supabase.SupabaseClient
 import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 
 
 @Composable
@@ -30,6 +21,8 @@ fun NavigationRoot(rootViewModel: RootViewModel = koinViewModel()) {
     val userHasAddress by rootViewModel.userHasAddress.collectAsStateWithLifecycle()
 
     // TODO: Remove
+    val client =
+        koinInject<SupabaseClient>()
     val context = LocalContext.current
 
     when (sessionStatus) {
@@ -46,20 +39,25 @@ fun NavigationRoot(rootViewModel: RootViewModel = koinViewModel()) {
 
         SessionStatus.SessionLoading -> SplashScreen()
         SessionStatus.Unauthenticated -> {
-            var show by remember { mutableStateOf(true) }
-            Box(Modifier.padding(48.dp)) {
-                Button("Show bottom sheet", onClick = { show = true })
-            }
-            val screenWidth = LocalWindowInfo.current.containerDpSize
-            val isFloating = screenWidth.width > 1200.dp || screenWidth.height > 1024.dp
-            // TODO: Handle better orientation
-            if (isFloating)
-                LocationEntryContent(koinViewModel<AddressViewModel>())
-            else
-                BottomSheet(show, onDismiss = { show = !show }) {
-                    LocationEntryContent(koinViewModel<AddressViewModel>(), isFloating = isFloating)
-                }
-//            AuthScreen(koinViewModel<AuthViewModel>())
+//            var show by remember { mutableStateOf(true) }
+//            Box(Modifier.padding(48.dp)) {
+//                Button("Show bottom sheet", onClick = { show = true })
+//            }
+//            val screenWidth = LocalWindowInfo.current.containerDpSize
+//            val isFloating = screenWidth.width > 1200.dp || screenWidth.height > 1024.dp
+//            // TODO: Handle better orientation
+//            if (isFloating)
+//                LocationEntryContent(koinViewModel<AddressViewModel>())
+//            else
+//                BottomSheet(show, onDismiss = { show = !show }) {
+//                    LocationEntryContent(koinViewModel<AddressViewModel>(), isFloating = isFloating)
+//                }
+//            runBlocking {
+//                val data = client.from(SupabaseTables.DISH)
+//                    .select(Columns.raw("id, title, description, ${SupabaseTables.DISH_CATEGORY}(category_name), ${SupabaseTables.NUTRITION_INFO}(calories, protein, carbs, fats)"))
+//                Log.d("Data", "$data")
+//            }
+            AuthScreen(koinViewModel<AuthViewModel>())
         }
     }
 }
--- scripts/src/supabase.ts
+++ scripts/src/supabase.ts
@@ -4,6 +4,6 @@ await process.loadEnvFile(".env");
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl!!, supabaseKey!!);
 
 export default supabase;
