9dfeea08ce9239da411cd1a164fad4b6c11f7c49
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-10 17:27:17 +0530

    Add OrderRepository skeleton

---
+++ app/src/main/java/com/littlelemon/application/orders/domain/OrderRepository.kt
@@ -0,0 +1,12 @@
+package com.littlelemon.application.orders.domain
+
+import com.littlelemon.application.core.domain.utils.Resource
+import com.littlelemon.application.orders.domain.models.CartItem
+import kotlinx.coroutines.flow.Flow
+
+interface OrderRepository {
+    fun getCartItems(): Flow<Resource<List<CartItem>>>
+    suspend fun placeOrder(): Resource<Unit> // TODO: Change to order item(details)
+    suspend fun cancelOrder(): Resource<Unit> // TODO: Change to order item
+    fun getAllOrders(): Flow<Resource<Unit>> // TODO: Change to OrderList with sorting option
+}
---
+++ app/src/main/java/com/littlelemon/application/orders/domain/models/CartItem.kt
@@ -0,0 +1,6 @@
+package com.littlelemon.application.orders.domain.models
+
+data class CartItem(
+//    private val menuItem: MenuItem , TODO: Add to common
+    val quantity: Int = 0
+)
