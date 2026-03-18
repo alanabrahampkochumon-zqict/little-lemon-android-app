a42e97efa2ff920203154630921c0052839011f3
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-18 14:19:12 +0530

    feat(MapHeader): Add fetch current location button

--- app/src/main/java/com/littlelemon/application/address/presentation/screens/components/MapHeader.kt
+++ app/src/main/java/com/littlelemon/application/address/presentation/screens/components/MapHeader.kt
@@ -1,17 +1,23 @@
 package com.littlelemon.application.address.presentation.screens.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,19 +35,28 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberUpdatedMarkerState
 import com.littlelemon.application.BuildConfig
 import com.littlelemon.application.R
+import com.littlelemon.application.address.presentation.AddressTestTags
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
 import com.littlelemon.application.core.presentation.designsystem.colors
 import com.littlelemon.application.core.presentation.designsystem.dimens
+import com.littlelemon.application.core.presentation.designsystem.shadows
 import com.littlelemon.application.core.presentation.designsystem.typeStyle
+import com.littlelemon.application.core.presentation.designsystem.xLarge
+import com.littlelemon.application.core.presentation.utils.toComposeShadow
 
 
 @Composable
 fun MapHeader(
-    onClose: () -> Unit,
     floatingBarTopPadding: Dp,
     floatingBarBottomPadding: Dp,
+    onClose: () -> Unit,
+    onFetchCurrentLocation: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+
+    val buttonShape = MaterialTheme.shapes.xLarge
+    val density = LocalDensity.current.density
+
     val singapore = LatLng(1.35, 103.87)
     val singaporeMarkerState = rememberUpdatedMarkerState(position = singapore)
     val cameraPositionState = rememberCameraPositionState {
@@ -53,13 +68,12 @@ fun MapHeader(
         modifier = modifier, contentAlignment = Alignment.TopCenter
     ) {
 
-
         if (BuildConfig.MAPS_API_KEY.isBlank())
             Box(
                 contentAlignment = Alignment.Center, modifier = Modifier
                     .fillMaxSize()
                     .background(
-                        MaterialTheme.colors.primaryDark
+                        MaterialTheme.colors.secondary
                     )
                     .padding(MaterialTheme.dimens.sizeXL)
             ) {
@@ -96,6 +110,22 @@ fun MapHeader(
                 )
             }
 
+        Box(modifier = Modifier.fillMaxSize().padding(MaterialTheme.dimens.sizeXL), contentAlignment = Alignment.BottomEnd) {
+            IconButton(
+                onClick = onFetchCurrentLocation,
+                modifier = Modifier
+                    .dropShadow(
+                        buttonShape,
+                        MaterialTheme.shadows.dropSM.firstShadow.toComposeShadow(density)
+                    )
+                    .background(MaterialTheme.colors.primary, buttonShape)
+                    .testTag(AddressTestTags.MAP_HEADER_FETCH_LOCATION_BUTTON)
+            ) {
+                Image(painterResource(R.drawable.ic_gpsfix), null)
+            }
+        }
+
+
         FloatingActionBar(
             stringResource(R.string.heading_add_your_address),
             onAction = onClose,
@@ -107,10 +137,10 @@ fun MapHeader(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun MapHeaderPreview() {
     LittleLemonTheme {
-        MapHeader(onClose = {}, floatingBarTopPadding = 0.dp, floatingBarBottomPadding = 0.dp)
+        MapHeader(onClose = {}, onFetchCurrentLocation = {}, floatingBarTopPadding = 16.dp, floatingBarBottomPadding = 0.dp)
     }
 }
