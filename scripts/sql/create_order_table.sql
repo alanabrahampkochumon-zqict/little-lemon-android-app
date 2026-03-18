9c2c30dbbe863c1e5d7d58115e2f666c3df7cf42
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-18 17:28:34 +0530

    fix(Scripts): fix order table schema bugs

--- scripts/sql/create_order_table.sql
+++ scripts/sql/create_order_table.sql
@@ -1,48 +1,34 @@
+CREATE TYPE ORDER_STATUS as ENUM ('ordered', 'delivered', 'cancelled');
+
 CREATE TABLE
     IF NOT EXISTS user_orders (
-        id UUID PRIMARY KEY DEFAULT get_random_uuid (),
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         user_id UUID NOT NULL REFERENCES auth.users (id) ON DELETE CASCADE DEFAULT auth.uid (),
         label TEXT NOT NULL,
         order_date TIMESTAMP DEFAULT now (),
         updated_at TIMESTAMP DEFAULT now (),
-        status ENUM ("ordered", "delivered", "cancelled") DEFAULT "ordered",
+        status ORDER_STATUS DEFAULT 'ordered',
         payment_mode VARCHAR(50) NOT NULL,
-        delivered_to UUID NOT NULL REFERENCES user_address.id ON DELETE CASCADE,
+        deliver_to UUID NOT NULL REFERENCES public.user_address (id) ON DELETE CASCADE,
         bill_amount BIGINT NOT NULL,
         delivery_charge BIGINT NOT NULL,
         discount BIGINT NOT NULL,
         total_payable BIGINT NOT NULL,
         refunded_on TIMESTAMP DEFAULT NULL,
-        special_instructions TEXT,
+        special_instructions TEXT
     );
 
 ---
 CREATE TABLE
     IF NOT EXISTS menu_order_item (
-        order_id UUID NOT NULL REFERENCES user_orders.id ON DELETE CASCADE,
-        dish_id UUID NOT NULL REFERENCES dish.id ON DELETE SET NULL,
+        order_id UUID NOT NULL REFERENCES public.user_orders (id) ON DELETE CASCADE,
+        dish_id UUID NOT NULL REFERENCES public.dish (id) ON DELETE SET NULL,
         dish_name_snapshot VARCHAR(255) NOT NULL,
         dish_price_snapshot BIGINT NOT NULL,
-        order_quantity INTEGER NOT NULL DEAFULT 1,
+        order_quantity INTEGER NOT NULL DEFAULT 1,
         PRIMARY KEY (order_id, dish_id)
     );
 
 CREATE POLICY "User can see their own orders" ON user_orders FOR
 SELECT
     USING (auth.uid () = user_id);
-
-CREATE OR REPLACE FUNCTION upd_timestamp() RETURNS TRIGGER
-LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    NEW.modified = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$;
-
-CREATE TRIGGER t_name
-  BEFORE UPDATE
-  ON tablename
-  FOR EACH ROW
-  EXECUTE PROCEDURE upd_timestamp();
