5c74d12926a99d8703c7d0cba4517b488ab96735
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-18 16:31:39 +0530

    fix(MainActivity): Maps composable crashing on older emulators

--- app/src/main/java/com/littlelemon/application/MainActivity.kt
+++ app/src/main/java/com/littlelemon/application/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.littlelemon.application
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.google.maps.GeoApiContext
-import com.google.maps.GeocodingApi
+import com.google.android.gms.maps.MapsInitializer
 import com.littlelemon.application.core.presentation.NavigationRoot
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
-import kotlinx.coroutines.runBlocking
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,21 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LittleLemonTheme {
                 NavigationRoot()
-                runBlocking {
-                    try {
-                        val context by inject<GeoApiContext>()
-                        val geocoderResult = GeocodingApi.geocode(
-                            context,
-                            "asdfasdfasdfasdfasfdasdfasfdasdfasdfasfa423142afsfdaf"
-                        ).await()
-//                        Log.d("Location", geocoderResult)
-                        Log.d("Location", geocoderResult.contentDeepToString())
-
-                    } catch (e: Exception) {
-                        Log.d("Location Exception", e.message.toString())
-                        Log.d("Location", e.toString())
-                    }
-                }
+                MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST) { }
             }
         }
     }
