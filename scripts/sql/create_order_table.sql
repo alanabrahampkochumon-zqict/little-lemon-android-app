5b59c67fb061143e06708cdb00d45ec3dbfbc5cc
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-18 17:14:46 +0530

    feat(Scripts): Add order table sql

---
+++ scripts/sql/create_order_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE
+    IF NOT EXISTS user_orders (
+        id UUID PRIMARY KEY DEFAULT get_random_uuid (),
+        user_id UUID NOT NULL REFERENCES auth.users (id) ON DELETE CASCADE DEFAULT auth.uid (),
+        label TEXT NOT NULL,
+        order_date TIMESTAMP DEFAULT now (),
+        updated_at TIMESTAMP DEFAULT ? ? ?,
+        status ENUM ("ordered", "delivered", "cancelled") DEFAULT "ordered",
+        payment_mode VARCHAR(50) NOT NULL,
+        delivered_to UUID NOT NULL REFERENCES user_address.id ON DELETE CASCADE,
+        bill_amount BIGINT NOT NULL,
+        delivery_charge BIGINT NOT NULL,
+        discount BIGINT NOT NULL,
+        total_payable BIGINT NOT NULL,
+        refunded_on TIMESTAMP DEFAULT NULL,
+        special_instructions TEXT,
+    );
+
+---
+CREATE TABLE
+    IF NOT EXISTS menu_order_item (
+        order_id UUID NOT NULL REFERENCES user_orders.id ON DELETE CASCADE,
+        dish_id UUID NOT NULL REFERENCES dish.id ON DELETE SET NULL,
+        dish_name_snapshot VARCHAR(255) NOT NULL,
+        dish_price_snapshot BIGINT NOT NULL,
+        order_quantity INTEGER NOT NULL DEAFULT 1,
+        PRIMARY KEY (order_id, dish_id)
+    );
+
+CREATE POLICY "User can see their own orders" ON user_orders FOR
+SELECT
+    USING (auth.uid () = user_id);
