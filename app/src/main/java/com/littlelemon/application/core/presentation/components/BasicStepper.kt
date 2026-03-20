01d792bc84cff58627379af3f435a669ead3c7cf
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-20 14:35:07 +0530

    feat: add basic stepper

---
+++ app/src/main/java/com/littlelemon/application/core/presentation/components/BasicStepper.kt
@@ -0,0 +1,113 @@
+package com.littlelemon.application.core.presentation.components
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.littlelemon.application.R
+import com.littlelemon.application.core.CoreTestTags
+import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
+import com.littlelemon.application.core.presentation.designsystem.colors
+import com.littlelemon.application.core.presentation.designsystem.dimens
+import com.littlelemon.application.core.presentation.designsystem.typeStyle
+
+@Composable
+fun BasicStepper(
+    value: Int,
+    onDecrease: () -> Unit,
+    onIncrease: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(
+            MaterialTheme.dimens.sizeSM
+        )
+    ) {
+        AnimatedVisibility(
+            value > 0, enter = fadeIn() + expandHorizontally(),
+            exit = fadeOut() + shrinkHorizontally(),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                BasicIconButton(
+                    R.drawable.ic_minus,
+                    onClick = onDecrease,
+                    iconDescription = stringResource(R.string.desc_decrease_quantity),
+                    modifier = Modifier.testTag(
+                        CoreTestTags.STEPPER_DECREASE
+                    ),
+                    colors = IconButtonColors(
+                        backgroundColor = MaterialTheme.colors.transparent,
+                        contentColor = MaterialTheme.colors.contentPlaceholder,
+                        disabledBackgroundColor = MaterialTheme.colors.disabled,
+                        disabledContentColor = MaterialTheme.colors.contentDisabled
+                    ),
+                )
+                AnimatedContent(targetState = value, transitionSpec = {
+                    if (targetState > initialState) {
+                        (slideInVertically { it } + fadeIn()) togetherWith slideOutVertically { -it } + fadeOut()
+                    } else {
+                        (slideInVertically { -it } + fadeIn()) togetherWith slideOutVertically { it } + fadeOut()
+                    }.using(SizeTransform(clip = false))
+                }) { targetValue ->
+                    Text(
+                        targetValue.toString(),
+                        style = MaterialTheme.typeStyle.displayMedium,
+                        color = MaterialTheme.colors.contentPlaceholder
+                    )
+                }
+            }
+        }
+        BasicIconButton(
+            R.drawable.ic_plus,
+            onClick = onIncrease,
+            iconDescription = stringResource(R.string.desc_increase_quantity),
+            modifier = Modifier.testTag(
+                CoreTestTags.STEPPER_INCREASE
+            ),
+            colors = IconButtonColors(
+                backgroundColor = MaterialTheme.colors.transparent,
+                contentColor = MaterialTheme.colors.contentTertiary,
+                disabledBackgroundColor = MaterialTheme.colors.disabled,
+                disabledContentColor = MaterialTheme.colors.contentDisabled
+            ),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BasicStepperPreview() {
+    val step = remember { mutableIntStateOf(5) }
+    LittleLemonTheme {
+        Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(12.dp)) {
+            BasicStepper(0, {}, {})
+            BasicStepper(step.intValue, { step.intValue-- }, { step.intValue++ })
+            BasicStepper(5, {}, {})
+        }
+    }
+}
