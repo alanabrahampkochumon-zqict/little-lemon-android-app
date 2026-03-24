fc509e26f3318247baf3a2955ac8dd02e08f5260
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-24 16:47:01 +0530

    feat: add header

---
+++ app/src/main/java/com/littlelemon/application/core/presentation/components/Header.kt
@@ -0,0 +1,69 @@
+package com.littlelemon.application.core.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
+import com.littlelemon.application.core.presentation.designsystem.colors
+import com.littlelemon.application.core.presentation.designsystem.dimens
+import com.littlelemon.application.core.presentation.designsystem.typeStyle
+
+enum class HeaderTypeStyle {
+    Primary,
+    Secondary
+}
+
+
+@Composable
+fun Header(
+    label: String,
+    modifier: Modifier = Modifier,
+    typeStyle: HeaderTypeStyle = HeaderTypeStyle.Primary,
+    actionContent: @Composable () -> Unit = {}
+) {
+    val style = if (typeStyle == HeaderTypeStyle.Primary) {
+        MaterialTheme.typeStyle.displaySmall
+    } else {
+        MaterialTheme.typeStyle.headlineSmall
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = MaterialTheme.dimens.sizeLG), verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(label, style = style, color = MaterialTheme.colors.contentPrimary, modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.width(MaterialTheme.dimens.sizeLG))
+        actionContent()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderPreview() {
+    LittleLemonTheme {
+        Column {
+            Header("Header") {
+                Text("action")
+            }
+            Spacer(Modifier.height(16.dp))
+            Header("Header", typeStyle = HeaderTypeStyle.Secondary) {
+                Text("action")
+            }
+        }
+    }
+}
