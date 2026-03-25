7a8e0a48c6cdd0e949c87b4328a08513da3c2ad1
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-25 16:16:39 +0530

    test: add physical address to full address mapper edge case tests

--- app/src/test/java/com/littlelemon/application/address/presentation/mappers/PhysicalAddressToFullAddressTest.kt
+++ app/src/test/java/com/littlelemon/application/address/presentation/mappers/PhysicalAddressToFullAddressTest.kt
@@ -12,6 +12,8 @@ class PhysicalAddressToFullAddressTest {
     private val pinCode = "123456"
 
     private val expectedFullAddress = "$buildingName, $streetAddress, $city, $state $pinCode"
+    private val noStatePinCodeAddress = "$buildingName, $streetAddress, $city"
+    private val noBuildingNameAddress = "$streetAddress, $city, $state $pinCode"
 
     @Test
     fun correctlyMapsAddress() {
@@ -29,6 +31,43 @@ class PhysicalAddressToFullAddressTest {
 
         // Then the address produces the expected result
         assertEquals(expectedFullAddress, fullAddress)
+    }
+
+
+    @Test
+    fun emptyLastFields_leavesNotTrailingCommas() {
+        // Given a physical address
+        val address = PhysicalAddress(
+            address = buildingName,
+            streetAddress = streetAddress,
+            city = city,
+            state = "",
+            pinCode = ""
+        )
+
+        // When mapped to full address
+        val fullAddress = address.toFullAddress()
+
+        // Then the address produces the expected result
+        assertEquals(noStatePinCodeAddress, fullAddress)
+    }
+
+    @Test
+    fun emptyFirstField_leavesNoLeadingComma() {
+        // Given a physical address
+        val address = PhysicalAddress(
+            address = "",
+            streetAddress = streetAddress,
+            city = city,
+            state = state,
+            pinCode = pinCode
+        )
+
+        // When mapped to full address
+        val fullAddress = address.toFullAddress()
+
+        // Then the address produces the expected result
+        assertEquals(noBuildingNameAddress, fullAddress)
     }
 
 }
