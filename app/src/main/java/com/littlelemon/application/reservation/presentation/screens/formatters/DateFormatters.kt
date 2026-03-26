05d96c3fb11ae82928f964495bf7d89f8708b79b
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-26 16:53:49 +0530

    feat: add datetime formatter

--- app/src/main/java/com/littlelemon/application/reservation/presentation/screens/formatters/DateFormatters.kt
+++ app/src/main/java/com/littlelemon/application/reservation/presentation/screens/formatters/DateFormatters.kt
@@ -3,7 +3,7 @@ package com.littlelemon.application.reservation.presentation.screens.formatters
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format.char
 
-fun LocalDateTime.formatTimeToTwelveHourClock(): String {
+fun LocalDateTime.formatTimeAsTwelveHourClock(): String {
     val format = LocalDateTime.Format {
         amPmHour()
         char(':')
---
+++ app/src/test/java/com/littlelemon/application/reservation/presentation/screens/formatters/DateFormatters.kt
@@ -0,0 +1,46 @@
+package com.littlelemon.application.reservation.presentation.screens.formatters
+
+import kotlinx.datetime.LocalDateTime
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DateFormatters {
+
+    @Test
+    fun correctlyFormatAMTime() {
+        // Given an AM datetime
+        val dateTime =
+            LocalDateTime(year = 2026, month = 12, day = 2, hour = 9, minute = 30, second = 35)
+        val expectedDateTime = "09:30 AM"
+        // When formatted
+        val formatted = dateTime.formatTimeAsTwelveHourClock()
+        // Then, it displays the correct time
+        assertEquals(expectedDateTime, formatted)
+    }
+
+    @Test
+    fun correctlyFormatsPMTime() {
+        // Given a PM datetime
+        val dateTime =
+            LocalDateTime(year = 2026, month = 12, day = 2, hour = 23, minute = 30, second = 35)
+        val expectedDateTime = "11:30 PM"
+        // When formatted
+        val formatted = dateTime.formatTimeAsTwelveHourClock()
+        // Then, it displays the correct time
+        assertEquals(expectedDateTime, formatted)
+    }
+
+    @Test
+    fun correctlyFormatsMidnightTime() {
+        // Given midnight hour
+        val dateTime =
+            LocalDateTime(year = 2026, month = 12, day = 2, hour = 0, minute = 0, second = 35)
+        val expectedDateTime = "12:00 AM"
+
+        // When formatted
+        val formatted = dateTime.formatTimeAsTwelveHourClock()
+        // Then, it displays the correct time
+        assertEquals(expectedDateTime, formatted)
+    }
+
+}
