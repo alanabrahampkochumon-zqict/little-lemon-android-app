23c654762b48a55c2247778c447a04bf9d8761ee
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-27 14:45:42 +0530

    feat: add time formatter for time distance

--- app/src/main/java/com/littlelemon/application/reservation/presentation/screens/components/ReservationCard.kt
+++ app/src/main/java/com/littlelemon/application/reservation/presentation/screens/components/ReservationCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
@@ -12,12 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.littlelemon.application.R
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
 import com.littlelemon.application.core.presentation.designsystem.colors
+import com.littlelemon.application.core.presentation.designsystem.shadows
 import com.littlelemon.application.core.presentation.designsystem.typeStyle
-import com.littlelemon.application.core.presentation.designsystem.xSmall
+import com.littlelemon.application.core.presentation.utils.toComposeShadow
 import com.littlelemon.application.reservation.domain.models.Reservation
 import com.littlelemon.application.reservation.presentation.screens.formatters.formatTimeAsTwelveHourClock
 import kotlinx.datetime.TimeZone
@@ -31,14 +38,32 @@ fun ReservationCard(
     onGetRoute: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val upcomingReservation =
+        reservation.reservationStatus == Reservation.ReservationStatus.Upcoming
     val dateTimeSize = 100.dp
+
     val contentColor =
-        if (reservation.reservationStatus == Reservation.ReservationStatus.Upcoming) MaterialTheme.colors.contentOnColor else MaterialTheme.colors.contentDisabled
+        if (upcomingReservation) MaterialTheme.colors.contentOnColor else MaterialTheme.colors.contentDisabled
     val backgroundColor =
-        if (reservation.reservationStatus == Reservation.ReservationStatus.Upcoming) MaterialTheme.colors.success else MaterialTheme.colors.disabled
+        if (upcomingReservation) MaterialTheme.colors.success else MaterialTheme.colors.disabled
+
     val innerCardShape = MaterialTheme.shapes.small
     val cardShape = MaterialTheme.shapes.medium
-    Row {
+
+    var monthName = reservation.reservedDate.date.month.name
+    monthName = monthName[0].uppercase() + monthName.substring(1, 3).lowercase()
+    val density = LocalDensity.current.density
+    Row(
+        modifier = modifier
+            .then(
+                if (upcomingReservation) Modifier.dropShadow(
+                    cardShape,
+                    MaterialTheme.shadows.dropSM.firstShadow.toComposeShadow(density)
+                ) else Modifier
+            )
+            .background(MaterialTheme.colors.primary, cardShape)
+            .clip(cardShape)
+    ) {
         Column(
             modifier = Modifier
                 .size(dateTimeSize)
@@ -47,26 +72,33 @@ fun ReservationCard(
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                reservation.reservedDate.date.month.name,
+                monthName,
                 color = contentColor,
                 style = MaterialTheme.typeStyle.headlineSmall
             )
             Text(
                 reservation.reservedDate.date.day.toString(),
                 color = contentColor,
-                style = MaterialTheme.typeStyle.displayLarge
+                style = MaterialTheme.typeStyle.displayLarge,
+                modifier = Modifier.offset(y = (-4).dp)
             )
             Text(
                 reservation.reservedDate.formatTimeAsTwelveHourClock(),
                 color = contentColor,
-                style = MaterialTheme.typeStyle.bodyXSmall
+                style = MaterialTheme.typeStyle.bodyXSmall,
+                modifier = Modifier.offset(y = (-6).dp)
             )
+        }
+
+        Column(modifier = Modifier.weight(1f)) {
+            Text(stringResource(R.string.reservation_card_title, reservation.reservedFor))
+
         }
     }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ReservationCardPreview() {
     val currentInstant = Instant.fromEpochMilliseconds(System.currentTimeMillis())
--- app/src/main/java/com/littlelemon/application/reservation/presentation/screens/formatters/DateFormatters.kt
+++ app/src/main/java/com/littlelemon/application/reservation/presentation/screens/formatters/DateFormatters.kt
@@ -1,7 +1,12 @@
 package com.littlelemon.application.reservation.presentation.screens.formatters
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
 import kotlinx.datetime.format.char
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 fun LocalDateTime.formatTimeAsTwelveHourClock(): String {
     val format = LocalDateTime.Format {
@@ -12,4 +17,32 @@ fun LocalDateTime.formatTimeAsTwelveHourClock(): String {
         amPmMarker("AM", "PM")
     }
     return format.format(this)
+}
+
+fun LocalDateTime.toTimeDistance(timezone: TimeZone = TimeZone.currentSystemDefault()): String {
+    val instant = toInstant(timezone)
+    val currentInstant = Clock.System.now()
+    if (instant > currentInstant) throw IllegalArgumentException("Cannot format future time")
+    val days = instant.daysUntil(currentInstant, timezone)
+    if (days < 1) {
+        val hours =
+            currentInstant.toLocalDateTime(timezone).hour - instant.toLocalDateTime(timezone).hour
+        if (hours < 1) {
+            val minutes =
+                currentInstant.toLocalDateTime(timezone).minute - instant.toLocalDateTime(timezone).minute
+            if (minutes < 1)
+                return "now"
+            else
+                return "$minutes minutes ago"
+        } else {
+            return "$hours hours ago"
+        }
+    } else if (days < 8) {
+        return "$days days ago"
+    } else if (days < 31) {
+        return "${days / 7} weeks ago"
+    } else if (days < 365) {
+        return "${days / 12} month(s) ago"
+    }
+    return "on ${month}-${day}-$year"
 }
