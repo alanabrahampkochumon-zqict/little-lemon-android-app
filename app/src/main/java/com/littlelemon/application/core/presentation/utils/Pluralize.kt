17cd312b31bb0a84639d788fdfc19b0e1c59fbc1
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-27 15:27:21 +0530

    feat: add pluralize

---
+++ app/src/main/java/com/littlelemon/application/core/presentation/utils/Pluralize.kt
@@ -0,0 +1,5 @@
+package com.littlelemon.application.core.presentation.utils
+
+fun pluralize(count: Int, singular: String, plural: String): String {
+    return "$count ${if (count > 1) plural else singular}"
+}
