d82998d912043583653e17fdfb63af4310a71ed1
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-07 14:37:57 +0530

    scripts/feat: add cart table sql

--- scripts/sql/create_cart_table.sql
+++ scripts/sql/create_cart_table.sql
@@ -1,5 +1,11 @@
 CREATE TABLE
-    IF NOT EXISTS user_cart (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    IF NOT EXISTS cart_item (
         user_id UUID NOT NULL REFERENCES auth.users (id) ON DELETE CASCADE DEFAULT auth.uid (),
+        dish_id UUID NOT NULL REFERENCES public.dish (id) ON DELETE CASCADE,
+        quantity INTEGER NOT NULL CHECK (quantity > 0) DEFAULT 1,
+        PRIMARY KEY (user_id, dish_id)
     );
+
+ALTER TABLE cart_item ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Users can manage their own cart items" ON cart_item FOR ALL USING (auth.uid () = user_id);
