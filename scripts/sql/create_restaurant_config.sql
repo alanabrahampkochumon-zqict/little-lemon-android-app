39b8c0771cfc0678fc890927c89963821fa6f7f8
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-07 15:43:50 +0530

    scripts/feat: add reservation config query

---
+++ scripts/sql/create_restaurant_config.sql
@@ -0,0 +1,29 @@
+CREATE TABLE
+    IF NOT EXISTS restaurant_config (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        location_name VARCHAR(255),
+        address TEXT,
+        lat_lng geography (POINT),
+        opening_time TIME,
+        closing_time TIME,
+        service_radius_miles float
+    );
+
+INSERT INTO
+    restaurant_config (
+        location_name,
+        address,
+        lat_lng,
+        opening_time,
+        closing_time,
+        service_radius_miles
+    )
+VALUES
+    (
+        'Little Lemon Chicago',
+        'Little Lemon, 2407 Vesta Drive, Illinois, Chicago-60626',
+        'POINT(-87.667010 41.903794)',
+        '10:00:00',
+        '22:00:00',
+        10.0
+    );
