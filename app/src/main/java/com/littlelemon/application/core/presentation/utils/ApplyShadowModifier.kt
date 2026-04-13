0bed09cb480ad8b8b4865575872e007f851ce040
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-13 09:49:50 +0530

    feat: add apply shadow helper

--- app/src/main/java/com/littlelemon/application/core/presentation/utils/ApplyShadowModifier.kt
+++ app/src/main/java/com/littlelemon/application/core/presentation/utils/ApplyShadowModifier.kt
@@ -2,8 +2,29 @@ package com.littlelemon.application.core.presentation.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.shadow.Shadow
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import com.littlelemon.application.core.presentation.designsystem.ShadowLayers
 
 @Composable
-fun Modifier.applyShadow() {
-
+fun Modifier.applyShadow(shape: Shape, shadows: ShadowLayers) {
+    shadows.layers.forEach { layer ->
+        this.dropShadow(
+            shape,
+            Shadow(
+                radius = layer.blurRadius.dp,
+                spread = layer.spread.dp,
+                offset = DpOffset(
+                    layer.xOffset.dp,
+                    layer.yOffset.dp
+                ),
+                color = Color(layer.color),
+                alpha = layer.opacity
+            )
+        )
+    }
 }
