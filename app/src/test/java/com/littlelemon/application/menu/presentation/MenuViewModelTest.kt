91d77879b075191cb56958eaeaff3da394d8c32a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-17 07:39:29 +0530

    refactor: group menu view model tests using junit5 nested tests classes

--- app/src/test/java/com/littlelemon/application/menu/presentation/MenuViewModelTest.kt
+++ app/src/test/java/com/littlelemon/application/menu/presentation/MenuViewModelTest.kt
@@ -53,177 +53,193 @@ class MenuViewModelTest {
         coEvery { useCase.invoke() } returns flow {
             emit(Resource.Success(dishes))
         }
-        coEvery { useCase.invoke(filter = DishFilter.INCLUDE_OUT_OF_STOCK) } returns flow {
-            emit(
-                Resource.Success(dishes + outOfStockDishes)
-            )
-        }
-        coEvery { useCase.invoke(filter = null) } returns flow {
-            emit(Resource.Success(dishes))
-        }
-        coEvery { useCase.invoke(sorting = DishSorting.NAME_ASCENDING) } returns flow {
-            emit(
-                Resource.Success(dishes.sortedBy { dish -> dish.title })
-            )
-        }
-        coEvery { useCase.invoke(sorting = DishSorting.NAME_DESCENDING) } returns flow {
-            emit(
-                Resource.Success(dishes.sortedByDescending { dish -> dish.title })
-            )
-        }
-
-        coEvery { useCase.invoke(forceFetch = true) } returns flow {
-            emit(
-                Resource.Success(remoteDishes)
-            )
-        }
-
-        coEvery { useCase.invoke(forceFetch = false) } returns flow {
-            emit(
-                Resource.Success(dishes)
-            )
-        }
 
         viewModel = MenuViewModel(useCase, testDispatcher)
     }
 
 
-    @Test
-    fun dishViewModel_onInitialization_stateIsSetToLoading() = runTest {
-        // Given viewmodel is created
-        viewModel.state.test {
-            // Then, initial state is loading
-            assertTrue(awaitItem().isLoading)
+    @Nested
+    inner class ViewModelInitialization {
+        @Test
+        fun initialStateIsSetToLoading() = runTest {
+            // Given viewmodel is created
+            viewModel.state.test {
+                // Then, initial state is loading
+                assertTrue(awaitItem().isLoading)
+            }
+
         }
 
-    }
+        @Test
+        fun getsDishesFromUseCase() = runTest {
+            // Given viewmodel is created
+            viewModel.state.test {
+                awaitItem() // Skips the initial loading
 
-    @Test
-    fun dishViewModel_onInitialization_getsDishesFromUseCase() = runTest {
-        // Given viewmodel is created
-        viewModel.state.test {
-            awaitItem() // Skips the initial loading
+                // Then, then result is populated
+                val state = awaitItem()
 
-            // Then, then result is populated
-            val state = awaitItem()
-
-            assertFalse(state.isLoading)
-            assertNull(state.error)
-            assertEquals(dishes, state.dishes)
-        }
-    }
-
-    @Test
-    fun onApplyFilter_outOfStockFilter_emitsDishesIncludingOutOfStock() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
-
-            // When, include out of stock filter is applied
-            viewModel.onAction(MenuActions.ApplyFiltering(filter = DishFilter.INCLUDE_OUT_OF_STOCK))
-
-            // Then, the result contains dishes including out of stock dishes
-            val state = awaitItem()
-            assertEquals(dishes + outOfStockDishes, state.dishes)
+                assertFalse(state.isLoading)
+                assertNull(state.error)
+                assertEquals(dishes, state.dishes)
+            }
         }
     }
 
-    @Test
-    fun onApplyFilter_noFilterApplied_emitsOnlyInStockDishes() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
 
-            // When, include out of stock filter is applied
-            viewModel.onAction(MenuActions.ApplyFiltering(filter = null))
-
-            // Then, the result contains dishes including out of stock dishes
-            val state = awaitItem()
-            assertEquals(dishes, state.dishes)
-        }
-    }
-
-    // TODO: Implementation
-//    @Test
-//    fun onApplySorting_showsLoadingInitially() = runTest {
-//        viewModel.state.test {
-//            awaitItem() // Skip the initial loading
-//
-//            // When, sort is applied
-//            viewModel.onAction(DishActions.ApplySorting(DishSorting.NAME_ASCENDING))
-//            // Then, the initial state is loading
-//            assertTrue(awaitItem().isLoading)
-//        }
-//    }
-
-    @Test
-    fun onApplySorting_sortedByNameAscending_emitsDishesSortedByNameAscending() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
-
-            // When, sort by name ascending is applied
-            viewModel.onAction(MenuActions.ApplySorting(DishSorting.NAME_ASCENDING))
-
-            // Then, the result contains dishes sorted by name ascending
-            val state = awaitItem()
-            assertNotNull(state.dishes)
-            assertTrue(state.dishes.zipWithNext { firstDish, secondDish -> firstDish.title <= secondDish.title }
-                .all { it })
-            assertFalse(state.isLoading) // and isLoading is false
-        }
-    }
-
-    @Test
-    fun onApplySorting_sortedByNameDescending_emitsDishesSortedByNameAscending() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
-
-            // When, sort by name ascending is applied
-            viewModel.onAction(MenuActions.ApplySorting(DishSorting.NAME_DESCENDING))
-
-            // Then, the result contains dishes sorted by name ascending
-            val state = awaitItem()
-            assertNotNull(state.dishes)
-            assertTrue(state.dishes.zipWithNext { firstDish, secondDish -> firstDish.title >= secondDish.title }
-                .all { it })
-            assertFalse(state.isLoading) // and isLoading is false
-        }
-    }
-
-    @Test
-    fun onFetchDishes_withFromRemote_fetchesItemFromRemote() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
-
-            // When forceFetch is called
-            viewModel.onAction(MenuActions.FetchDishes(true))
-
-            // Then, items are fetched from remote
-            val state = awaitItem()
-            assertNotNull(state.dishes)
-            assertEquals(remoteDishes, state.dishes)
-        }
-    }
-
-    @Test
-    fun onFetchDishes_withoutFromRemote_fetchesItemFromCache() = runTest {
-        viewModel.state.test {
-            awaitItem() // Skip the initial loading
-
-            // When forceFetch is called
-            viewModel.onAction(MenuActions.FetchDishes(false))
-
-            // Then, items are fetched from cache
-            val state = awaitItem()
-            assertNotNull(state.dishes)
-            assertEquals(dishes, state.dishes)
-        }
-    }
-
-    // TODO: Refactor above tests in subtests
     @Nested
     inner class Actions {
 
+        @BeforeEach
+        fun setUp() {
+            coEvery { useCase.invoke(forceFetch = true) } returns flow {
+                emit(
+                    Resource.Success(remoteDishes)
+                )
+            }
+
+            coEvery { useCase.invoke(forceFetch = false) } returns flow {
+                emit(
+                    Resource.Success(dishes)
+                )
+            }
+            viewModel = MenuViewModel(useCase, testDispatcher)
+        }
+
         @Nested
-        inner class UpdateCategoryAction {
+        inner class ApplyFilter {
+
+            @BeforeEach
+            fun setUp() {
+                coEvery { useCase.invoke(filter = DishFilter.INCLUDE_OUT_OF_STOCK) } returns flow {
+                    emit(
+                        Resource.Success(dishes + outOfStockDishes)
+                    )
+                }
+                coEvery { useCase.invoke(filter = null) } returns flow {
+                    emit(Resource.Success(dishes))
+                }
+                viewModel = MenuViewModel(useCase, testDispatcher)
+            }
+
+            @Test
+            fun outOfStockFilter_emitsDishesIncludingOutOfStock() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When, include out of stock filter is applied
+                    viewModel.onAction(MenuActions.ApplyFiltering(filter = DishFilter.INCLUDE_OUT_OF_STOCK))
+
+                    // Then, the result contains dishes including out of stock dishes
+                    val state = awaitItem()
+                    assertEquals(dishes + outOfStockDishes, state.dishes)
+                }
+            }
+
+            @Test
+            fun noFilterApplied_emitsOnlyInStockDishes() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When, include out of stock filter is applied
+                    viewModel.onAction(MenuActions.ApplyFiltering(filter = null))
+
+                    // Then, the result contains dishes including out of stock dishes
+                    val state = awaitItem()
+                    assertEquals(dishes, state.dishes)
+                }
+            }
+        }
+
+        @Nested
+        inner class ApplySorting {
+
+            @BeforeEach
+            fun setUp() {
+                coEvery { useCase.invoke(sorting = DishSorting.NAME_ASCENDING) } returns flow {
+                    emit(
+                        Resource.Success(dishes.sortedBy { dish -> dish.title })
+                    )
+                }
+                coEvery { useCase.invoke(sorting = DishSorting.NAME_DESCENDING) } returns flow {
+                    emit(
+                        Resource.Success(dishes.sortedByDescending { dish -> dish.title })
+                    )
+                }
+                viewModel = MenuViewModel(useCase, testDispatcher)
+            }
+
+
+            @Test
+            fun sortedByNameAscending_emitsDishesSortedByNameAscending() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When, sort by name ascending is applied
+                    viewModel.onAction(MenuActions.ApplySorting(DishSorting.NAME_ASCENDING))
+
+                    // Then, the result contains dishes sorted by name ascending
+                    val state = awaitItem()
+                    assertNotNull(state.dishes)
+                    assertTrue(state.dishes.zipWithNext { firstDish, secondDish -> firstDish.title <= secondDish.title }
+                        .all { it })
+                    assertFalse(state.isLoading) // and isLoading is false
+                }
+            }
+
+            @Test
+            fun sortedByNameDescending_emitsDishesSortedByNameAscending() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When, sort by name ascending is applied
+                    viewModel.onAction(MenuActions.ApplySorting(DishSorting.NAME_DESCENDING))
+
+                    // Then, the result contains dishes sorted by name ascending
+                    val state = awaitItem()
+                    assertNotNull(state.dishes)
+                    assertTrue(state.dishes.zipWithNext { firstDish, secondDish -> firstDish.title >= secondDish.title }
+                        .all { it })
+                    assertFalse(state.isLoading) // and isLoading is false
+                }
+            }
+        }
+
+        @Nested
+        inner class FetchDishes {
+            @Test
+            fun withFromRemote_fetchesItemFromRemote() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When forceFetch is called
+                    viewModel.onAction(MenuActions.FetchDishes(true))
+
+                    // Then, items are fetched from remote
+                    val state = awaitItem()
+                    assertNotNull(state.dishes)
+                    assertEquals(remoteDishes, state.dishes)
+                }
+            }
+
+            @Test
+            fun withoutFromRemote_fetchesItemFromCache() = runTest {
+                viewModel.state.test {
+                    awaitItem() // Skip the initial loading
+
+                    // When forceFetch is called
+                    viewModel.onAction(MenuActions.FetchDishes(false))
+
+                    // Then, items are fetched from cache
+                    val state = awaitItem()
+                    assertNotNull(state.dishes)
+                    assertEquals(dishes, state.dishes)
+                }
+            }
+        }
+
+        @Nested
+        inner class UpdateCategory {
             private val filterCategory = "CATEGORY"
             private val categoryFilteredDishes =
                 DishGenerator.generateDishWithCategories(5)
