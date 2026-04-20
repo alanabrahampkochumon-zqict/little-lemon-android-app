d3f0f71c1554a66c858f28d60ab62181905d68db
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-20 17:12:20 +0530

    fix: update reservation card style to match new design guidelines

--- app/src/main/java/com/littlelemon/application/reservation/presentation/screens/components/ReservationCard.kt
+++ app/src/main/java/com/littlelemon/application/reservation/presentation/screens/components/ReservationCard.kt
@@ -1,7 +1,6 @@
 package com.littlelemon.application.reservation.presentation.screens.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,12 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +27,6 @@ import com.littlelemon.application.core.presentation.components.ButtonVariant
 import com.littlelemon.application.core.presentation.components.Tag
 import com.littlelemon.application.core.presentation.components.TagVariant
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
-import com.littlelemon.application.core.presentation.designsystem.typeStyle
 import com.littlelemon.application.core.presentation.utils.applyShadow
 import com.littlelemon.application.core.presentation.utils.toTitleCase
 import com.littlelemon.application.reservation.domain.models.Reservation
@@ -53,23 +51,22 @@ fun ReservationCard(
         if (upcomingReservation) LittleLemonTheme.colors.contentOnColor else LittleLemonTheme.colors.contentDisabled
     val backgroundColor =
         if (upcomingReservation) LittleLemonTheme.colors.success else LittleLemonTheme.colors.disabled
-    val borderColor =
-        if (upcomingReservation) LittleLemonTheme.colors.transparent else LittleLemonTheme.colors.outlineDisabled
 
     val tagVariant = if (upcomingReservation) TagVariant.SuccessLight else TagVariant.DisabledLight
 
-    val innerCardShape = LittleLemonTheme.shapes.xs
-    val cardShape = LittleLemonTheme.shapes.xs
+    val innerCardShape =
+        LittleLemonTheme.shapes.md.copy(topEnd = CornerSize(12.dp), bottomEnd = CornerSize(12.dp))
+    val cardShape = LittleLemonTheme.shapes.md
 
     val monthName = reservation.reservationDate.date.month.name.substring(0, 3).toTitleCase()
-    val density = LocalDensity.current.density
+
+    val fontOffset = -LittleLemonTheme.dimens.sizeMD
 
     Row(
         modifier = modifier
             .applyShadow(cardShape, LittleLemonTheme.shadows.dropSM)
             .widthIn(max = 440.dp, min = 300.dp)
             .background(LittleLemonTheme.colors.primary, cardShape)
-            .border(1.dp, borderColor, cardShape)
             .clip(cardShape)
     ) {
         Column(
@@ -82,26 +79,26 @@ fun ReservationCard(
             Text(
                 monthName,
                 color = contentColor,
-                style = LittleLemonTheme.typography.headlineSmall
+                style = LittleLemonTheme.typography.displaySmall
             )
             Text(
                 reservation.reservationDate.date.day.toString(),
                 color = contentColor,
                 style = LittleLemonTheme.typography.displayLarge,
-                modifier = Modifier.offset(y = (-4).dp)
+                modifier = Modifier.offset(y = fontOffset)
             )
             Text(
                 reservation.reservationDate.formatTimeAsTwelveHourClock(),
                 color = contentColor,
-                style = LittleLemonTheme.typography.bodyXSmall,
-                modifier = Modifier.offset(y = (-6).dp)
+                style = LittleLemonTheme.typography.bodySmall,
+                modifier = Modifier.offset(y = fontOffset)
             )
         }
 
         Column(
             modifier = Modifier
                 .padding(
-                    top = LittleLemonTheme.dimens.sizeMD,
+                    top = LittleLemonTheme.dimens.sizeLG,
                     start = LittleLemonTheme.dimens.sizeLG,
                     end = LittleLemonTheme.dimens.sizeMD
                 )
