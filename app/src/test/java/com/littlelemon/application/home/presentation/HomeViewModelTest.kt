6845fb22bc98320aeaeec812ddcd9ec6493b61ab
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-21 16:51:24 +0530

    test: add homeviewmodel dish state tests

--- app/src/test/java/com/littlelemon/application/home/presentation/HomeViewModelTest.kt
+++ app/src/test/java/com/littlelemon/application/home/presentation/HomeViewModelTest.kt
@@ -1,22 +1,28 @@
 package com.littlelemon.application.home.presentation
 
 import app.cash.turbine.test
-import com.littlelemon.application.address.domain.models.LocalAddress
 import com.littlelemon.application.address.domain.usecase.GetAddressUseCase
 import com.littlelemon.application.address.utils.AddressGenerator
 import com.littlelemon.application.core.domain.utils.Resource
 import com.littlelemon.application.menu.domain.usecase.GetCategoriesUseCase
 import com.littlelemon.application.menu.domain.usecase.GetDishesUseCase
+import com.littlelemon.application.menu.utils.DishGenerator
+import com.littlelemon.application.utils.StandardTestDispatcherRule
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertNull
+import org.junit.jupiter.api.extension.ExtendWith
 import kotlin.test.assertEquals
-import kotlin.test.assertIs
+import kotlin.test.assertTrue
 
+@ExtendWith(StandardTestDispatcherRule::class)
 class HomeViewModelTest {
 
     private lateinit var getAddressUseCase: GetAddressUseCase
@@ -24,6 +30,8 @@ class HomeViewModelTest {
     private lateinit var getCategoriesUseCase: GetCategoriesUseCase
     private lateinit var viewModel: HomeViewModel
     private val addresses = List(3) { AddressGenerator.generateLocalAddress() }
+    private val dishes = List(5) { DishGenerator.generateDish() }
+    private val categories = dishes.flatMap { dish -> dish.category }.distinct()
 
     @BeforeEach
     fun setUp() {
@@ -36,18 +44,28 @@ class HomeViewModelTest {
             emit(Resource.Success(addresses))
         }
 
+        coEvery { getDishesUseCase.invoke(any(), any(), any(), any()) } returns flow {
+            emit(Resource.Loading(null))
+            emit(Resource.Success(dishes))
+        }
+
+        coEvery { getCategoriesUseCase.invoke() } returns flow {
+            emit(Resource.Loading(null))
+            emit(Resource.Success(categories))
+        }
+
         viewModel = HomeViewModel(getAddressUseCase, getDishesUseCase, getCategoriesUseCase)
     }
 
 
     @Nested
-    inner class AddressTests {
+    inner class AddressState {
 
         @Test
         fun initiallyEmitsLoadingState() = runTest {
             viewModel.state.test {
-                val loadingState = awaitItem()
-//                assertTrue { loadingState.is }
+                val state = awaitItem()
+                assertTrue { state.addressLoading }
                 cancelAndConsumeRemainingEvents()
             }
         }
@@ -58,8 +76,9 @@ class HomeViewModelTest {
                 skipItems(1)
 
                 val successState = awaitItem()
-                assertIs<Resource.Success<List<LocalAddress>>>(successState)
-                assertEquals(addresses, successState.data)
+                assertEquals(addresses, successState.addresses)
+                assertNull(successState.addressError)
+                Assertions.assertFalse(successState.addressLoading)
             }
         }
 
@@ -72,11 +91,100 @@ class HomeViewModelTest {
             viewModel.state.test {
                 skipItems(1)
 
-                // Then a failure state is emitted.
                 val failureState = awaitItem()
-                assertIs<Resource.Failure<List<LocalAddress>>>(failureState)
+                // Then error state is not null
+                assertNotNull(failureState.addressError)
+                // And loading state is false
+                Assertions.assertFalse(failureState.addressLoading)
+                cancelAndConsumeRemainingEvents()
             }
         }
     }
 
+    @Nested
+    inner class DishesState {
+
+        @Test
+        fun initiallyEmitsLoadingState() = runTest {
+            viewModel.state.test {
+                val state = awaitItem()
+                assertTrue { state.dishLoading }
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+
+        @Test
+        fun onSuccess_emitsSuccessResourceWithData() = runTest {
+            viewModel.state.test {
+                skipItems(1)
+
+                val successState = awaitItem()
+                assertEquals(dishes, successState.popularDishes)
+                assertNull(successState.dishError)
+                Assertions.assertFalse(successState.dishLoading)
+            }
+        }
+
+        @Test
+        fun onFailure_emitsFailureResource() = runTest {
+            // When a usecase failure occurs
+            coEvery { getDishesUseCase.invoke() } returns flow { emit(Resource.Failure()) }
+            viewModel = HomeViewModel(getAddressUseCase, getDishesUseCase, getCategoriesUseCase)
+
+            viewModel.state.test {
+                skipItems(1)
+
+                val failureState = awaitItem()
+                // Then error state is not null
+                assertNotNull(failureState.dishError)
+                // And loading state is false
+                Assertions.assertFalse(failureState.dishLoading)
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+    }
+
+
+    @Nested
+    inner class CategoryState {
+
+        @Test
+        fun initiallyEmitsLoadingState() = runTest {
+            viewModel.state.test {
+                val state = awaitItem()
+                assertTrue { state.categoryLoading }
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+
+        @Test
+        fun onSuccess_emitsSuccessResourceWithData() = runTest {
+            viewModel.state.test {
+                skipItems(1)
+
+                val successState = awaitItem()
+                assertEquals(categories, successState.categories)
+                assertNull(successState.categoryError)
+                Assertions.assertFalse(successState.categoryLoading)
+            }
+        }
+
+        @Test
+        fun onFailure_emitsFailureResource() = runTest {
+            // When a usecase failure occurs
+            coEvery { getCategoriesUseCase.invoke() } returns flow { emit(Resource.Failure()) }
+            viewModel = HomeViewModel(getAddressUseCase, getDishesUseCase, getCategoriesUseCase)
+
+            viewModel.state.test {
+                skipItems(1)
+
+                val failureState = awaitItem()
+                // Then error state is not null
+                assertNotNull(failureState.categoryError)
+                // And loading state is false
+                Assertions.assertFalse(failureState.categoryLoading)
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+    }
 }
