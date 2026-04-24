0cb92989fe008d5bb1f18f67e0ea0e588b676fb7
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-24 15:58:06 +0530

    feat: add address card tag, fix address card spacing

--- app/src/main/java/com/littlelemon/application/core/presentation/components/AddressCard.kt
+++ app/src/main/java/com/littlelemon/application/core/presentation/components/AddressCard.kt
@@ -3,14 +3,17 @@ package com.littlelemon.application.core.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.littlelemon.application.R
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
 import com.littlelemon.application.core.presentation.utils.applyShadow
 
@@ -25,18 +28,34 @@ fun AddressCard(
     onDelete: () -> Unit = {},
     onEdit: () -> Unit = {},
 ) {
+
+    val tagVariant = if (selected) TagVariant.SuccessLight else TagVariant.SuccessFilled
     Column(
         modifier
-            .applyShadow( LittleLemonTheme.shapes.sm, LittleLemonTheme.shadows.dropXS)
+            .applyShadow(LittleLemonTheme.shapes.sm, LittleLemonTheme.shadows.dropXS)
             .background(LittleLemonTheme.colors.primary, LittleLemonTheme.shapes.sm)
             .padding(
-                vertical = LittleLemonTheme.dimens.sizeXL,
-                horizontal = LittleLemonTheme.dimens.sizeLG
+                vertical = LittleLemonTheme.dimens.sizeLG,
+                horizontal = LittleLemonTheme.dimens.sizeXL
             )
     ) {
-        Text(addressLabel, style = LittleLemonTheme.typography.displaySmall, color = LittleLemonTheme.colors.contentSecondary)
+        Text(
+            addressLabel,
+            style = LittleLemonTheme.typography.displaySmall,
+            color = LittleLemonTheme.colors.contentSecondary
+        )
         Spacer(modifier = Modifier.height(LittleLemonTheme.dimens.sizeXS))
-        Text(fullAddress, style = LittleLemonTheme.typography.bodySmall, color = LittleLemonTheme.colors.contentTertiary)
+        Text(
+            fullAddress,
+            style = LittleLemonTheme.typography.bodySmall,
+            color = LittleLemonTheme.colors.contentTertiary
+        )
+        Spacer(modifier = Modifier.height(LittleLemonTheme.dimens.sizeLG))
+        Row() {
+            if (default)
+                Tag(text = stringResource(R.string.default_tag), variant = tagVariant)
+            // TODO: Add actions
+        }
     }
 }
 
@@ -46,7 +65,10 @@ fun AddressCard(
 private fun AddressCardPreview() {
 
     LittleLemonTheme {
-        Column(verticalArrangement = Arrangement.spacedBy(12.dp), modifier = Modifier.padding(12.dp)) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.padding(12.dp)
+        ) {
             AddressCard(
                 "Workplace", "16281 Washington Avenue,\nLincoln Park,\nChicago - 60614", true, true
             )
@@ -54,7 +76,10 @@ private fun AddressCardPreview() {
                 "Workplace", "16281 Washington Avenue,\nLincoln Park,\nChicago - 60614", true, false
             )
             AddressCard(
-                "Workplace", "16281 Washington Avenue,\nLincoln Park,\nChicago - 60614", false, false
+                "Workplace",
+                "16281 Washington Avenue,\nLincoln Park,\nChicago - 60614",
+                false,
+                false
             )
         }
     }
