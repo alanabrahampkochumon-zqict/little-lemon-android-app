73c513966243256e27a611775ff56897fa9efef5
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-24 17:50:21 +0530

    scripts/refactor: update comments in upsert cart rpc

--- scripts/sql/rpc/add_remove_cart_item.rpc.sql
+++ scripts/sql/rpc/add_remove_cart_item.rpc.sql
@@ -6,7 +6,7 @@ RETURNS cart_item LANGUAGE plpgsql AS $$
 DECLARE
     d_cart_item cart_item;
 BEGIN
-    -- If the quantity is 0, then delete it from database and return null
+    -- If the quantity is less than or equal to 0, then delete it from database and return null
     IF p_quantity <= 0 THEN 
         DELETE FROM cart_item WHERE user_id = auth.uid() AND dish_id = p_dish_id;
         RETURN NULL;
