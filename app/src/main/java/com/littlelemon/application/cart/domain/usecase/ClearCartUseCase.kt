d94e089063c8fce618ebf852855fd34745d49967
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-29 17:26:34 +0530

    feat: add ClearCartUseCase

---
+++ app/src/main/java/com/littlelemon/application/cart/domain/usecase/ClearCartUseCase.kt
@@ -0,0 +1,8 @@
+package com.littlelemon.application.cart.domain.usecase
+
+import com.littlelemon.application.cart.domain.CartRepository
+
+class ClearCartUseCase(private val repository: CartRepository) {
+
+    suspend operator fun invoke() = repository.clearCart()
+}
--- app/src/main/java/com/littlelemon/application/cart/domain/usecase/UpsertCartItemUseCase.kt
+++ app/src/main/java/com/littlelemon/application/cart/domain/usecase/UpsertCartItemUseCase.kt
@@ -4,6 +4,6 @@ import com.littlelemon.application.cart.domain.CartRepository
 import com.littlelemon.application.cart.domain.models.CartItem
 
 class UpsertCartItemUseCase(private val repository: CartRepository) {
-
+    // TODO: Add tests
     suspend operator fun invoke(cartItem: CartItem) = repository.upsertCartItem(cartItem)
 }
