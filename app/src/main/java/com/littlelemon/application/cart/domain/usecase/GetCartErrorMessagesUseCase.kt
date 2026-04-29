36a9759427dec1dca6745451a9522b2773c97e58
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-29 17:27:37 +0530

    feat: add GetCartErrorMessagesUseCase

---
+++ app/src/main/java/com/littlelemon/application/cart/domain/usecase/GetCartErrorMessagesUseCase.kt
@@ -0,0 +1,8 @@
+package com.littlelemon.application.cart.domain.usecase
+
+import com.littlelemon.application.cart.domain.CartRepository
+import kotlinx.coroutines.flow.SharedFlow
+
+class GetCartErrorMessagesUseCase(private val repository: CartRepository) {
+    operator fun invoke(): SharedFlow<String> = repository.errorMessages
+}
