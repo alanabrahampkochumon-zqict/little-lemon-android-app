765913bac982afe3aaeb705fc8a82a61a573c6a2
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-29 20:19:28 +0530

    feat: add errorMessage state to CartViewModel

--- app/src/main/java/com/littlelemon/application/cart/presentation/CartViewModel.kt
+++ app/src/main/java/com/littlelemon/application/cart/presentation/CartViewModel.kt
@@ -2,13 +2,17 @@ package com.littlelemon.application.cart.presentation
 
 import androidx.lifecycle.ViewModel
 import com.littlelemon.application.cart.domain.usecase.ClearCartUseCase
+import com.littlelemon.application.cart.domain.usecase.GetCartErrorMessagesUseCase
 import com.littlelemon.application.cart.domain.usecase.GetCartItemsUseCase
 import com.littlelemon.application.cart.domain.usecase.UpsertCartItemUseCase
+import kotlinx.coroutines.flow.SharedFlow
 
 class CartViewModel(
     private val getCartItems: GetCartItemsUseCase,
-    private val getCartError: GetCartItemsUseCase,
+    private val getCartError: GetCartErrorMessagesUseCase,
     private val upsertCartItem: UpsertCartItemUseCase,
     private val clearCart: ClearCartUseCase
 ) : ViewModel() {
+
+    val errorState: SharedFlow<String> = getCartError()
 }
---
+++ app/src/test/java/com/littlelemon/application/cart/presentation/CartViewModelTest.kt
@@ -0,0 +1,78 @@
+package com.littlelemon.application.cart.presentation
+
+import app.cash.turbine.test
+import com.littlelemon.application.cart.domain.models.CartItem
+import com.littlelemon.application.cart.domain.usecase.ClearCartUseCase
+import com.littlelemon.application.cart.domain.usecase.GetCartErrorMessagesUseCase
+import com.littlelemon.application.cart.domain.usecase.GetCartItemsUseCase
+import com.littlelemon.application.cart.domain.usecase.UpsertCartItemUseCase
+import com.littlelemon.application.core.domain.utils.Resource
+import com.littlelemon.application.menu.utils.DishGenerator
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class CartViewModelTest {
+
+    private lateinit var errorMessageUseCase: GetCartErrorMessagesUseCase
+    private lateinit var getItemUseCase: GetCartItemsUseCase
+    private lateinit var upsertUseCase: UpsertCartItemUseCase
+    private lateinit var clearCartUseCase: ClearCartUseCase
+
+    private lateinit var testScope: TestScope
+
+    private lateinit var viewModel: CartViewModel
+
+
+    private val errorMessage = "ERROR MESSAGE"
+    private val cartItems = List(5) {
+        val dish = DishGenerator.generateDish()
+        CartItem(dish, Random.nextInt(3, 5))
+    }
+
+
+    @BeforeEach
+    fun setUp() {
+        errorMessageUseCase = mockk()
+        getItemUseCase = mockk()
+        upsertUseCase = mockk()
+        clearCartUseCase = mockk()
+
+        testScope = TestScope(StandardTestDispatcher())
+
+        coEvery { errorMessageUseCase.invoke() } returns flow { emit(errorMessage) }.shareIn(
+            testScope,
+            SharingStarted.Eagerly, 1
+        )
+        coEvery { getItemUseCase.invoke() } returns flow { emit(cartItems) }
+        coEvery { upsertUseCase(any()) } returns Unit
+        coEvery { clearCartUseCase.invoke() } returns Resource.Success()
+
+        viewModel =
+            CartViewModel(getItemUseCase, errorMessageUseCase, upsertUseCase, clearCartUseCase)
+
+    }
+
+    @Nested
+    inner class ErrorState {
+
+        @Test
+        fun emitsErrorState_whenErrorMessageIsInQueue() = runTest(testScope.testScheduler) {
+            viewModel.errorState.test {
+                val message = awaitItem()
+                assertEquals(errorMessage, message)
+            }
+        }
+    }
+
+}
