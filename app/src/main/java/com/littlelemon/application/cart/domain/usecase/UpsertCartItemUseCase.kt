c5aea05d754cbf3c9c6f675e1da29476a2b49073
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-29 17:25:41 +0530

    feat: add UpsertCartItemUseCase

---
+++ app/src/main/java/com/littlelemon/application/cart/domain/usecase/UpsertCartItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.littlelemon.application.cart.domain.usecase
+
+import com.littlelemon.application.cart.domain.CartRepository
+import com.littlelemon.application.cart.domain.models.CartItem
+
+class UpsertCartItemUseCase(private val repository: CartRepository) {
+
+    suspend operator fun invoke(cartItem: CartItem) = repository.upsertCartItem(cartItem)
+}
