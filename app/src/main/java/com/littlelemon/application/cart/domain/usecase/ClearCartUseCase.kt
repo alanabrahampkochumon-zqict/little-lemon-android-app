d028dfc8b8a7f4b18736d6b358be08fcd288c60d
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-29 19:49:00 +0530

    test: add ClearCartUseCase to verify success case

--- app/src/main/java/com/littlelemon/application/cart/domain/usecase/ClearCartUseCase.kt
+++ app/src/main/java/com/littlelemon/application/cart/domain/usecase/ClearCartUseCase.kt
@@ -3,6 +3,5 @@ package com.littlelemon.application.cart.domain.usecase
 import com.littlelemon.application.cart.domain.CartRepository
 
 class ClearCartUseCase(private val repository: CartRepository) {
-
     suspend operator fun invoke() = repository.clearCart()
 }
