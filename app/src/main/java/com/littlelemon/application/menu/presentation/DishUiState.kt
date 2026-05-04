6bbf3de7987f407811530b6ca40bb937c0817437
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-04 16:25:57 +0530

    refactor: rename MenuScreenUiState to DishUiState to ensure there are no confusion among ui states

--- app/src/main/java/com/littlelemon/application/menu/presentation/DishUiState.kt
+++ app/src/main/java/com/littlelemon/application/menu/presentation/DishUiState.kt
@@ -2,7 +2,7 @@ package com.littlelemon.application.menu.presentation
 
 import com.littlelemon.application.shared.menu.domain.models.Dish
 
-data class MenuScreenUiState(
+data class DishUiState(
     val dish: Dish,
     val quantity: Int = 0
 )
