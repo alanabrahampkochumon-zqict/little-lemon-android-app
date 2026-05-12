ba396a674d16547ced867afe554eb030d2d493a0
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-12 18:30:25 +0530

    feat: add basic address card

---
+++ app/src/main/java/com/littlelemon/application/shared/address/presentation/AddressCard.kt
@@ -0,0 +1,97 @@
+package com.littlelemon.application.shared.address.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.littlelemon.application.R
+import com.littlelemon.application.address.domain.models.LocalAddress
+import com.littlelemon.application.address.domain.models.PhysicalAddress
+import com.littlelemon.application.core.presentation.components.Tag
+import com.littlelemon.application.core.presentation.components.TagVariant
+import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
+import com.littlelemon.application.core.presentation.utils.applyShadow
+
+
+@Composable
+fun AddressCard(address: LocalAddress, modifier: Modifier = Modifier) {
+
+    val shape = LittleLemonTheme.shapes.lg
+    val addressString = if (address.address != null) {
+        "${address.address.address},\n${address.address.streetAddress},\n${address.address.city}\n${address.address.state} - ${address.address.pinCode}"
+    } else if (address.location != null) {
+        "(${address.location.latitude}, ${address.location.longitude})"
+    } else {
+        stringResource(R.string.unknown_location)
+    }
+
+    Column(
+        modifier = Modifier
+            .applyShadow(shape, LittleLemonTheme.shadows.dropSM)
+            .background(LittleLemonTheme.colors.primary, shape)
+            .padding(
+                start = LittleLemonTheme.dimens.sizeXL,
+                top = LittleLemonTheme.dimens.sizeLG
+            )
+    ) {
+        Text(
+            address.label ?: stringResource(R.string.unnamed_address_label),
+            style = LittleLemonTheme.typography.displaySmall
+        )
+        Spacer(Modifier.height(LittleLemonTheme.dimens.sizeSM))
+        Text(addressString, style = LittleLemonTheme.typography.bodySmall)
+//        Spacer(Modifier.height(LittleLemonTheme.dimens.sizeLG))
+        Row {
+            if (address.isDefault)
+                Tag(stringResource(R.string.default_tag), variant = TagVariant.SuccessFilled)
+
+            Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.End) {
+                Box(modifier = Modifier.minimumInteractiveComponentSize()) {
+                    Image(
+                        painterResource(R.drawable.ic_trash),
+                        contentDescription = stringResource(R.string.delete_address)
+                    )
+                }
+// TODO: Update image
+                Box(modifier = Modifier.minimumInteractiveComponentSize()) {
+                    Image(
+                        painterResource(R.drawable.ic_pencilsimple),
+                        contentDescription = stringResource(R.string.delete_address)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun AddressCardPreview() {
+
+    LittleLemonTheme {
+        AddressCard(
+            address = LocalAddress(
+                id = "1234", label = "Physical Address", address = PhysicalAddress(
+                    address = "Some street",
+                    streetAddress = "some street address",
+                    city = "Some city",
+                    state = "Some state",
+                    pinCode = "12349324"
+                )
+            )
+        )
+    }
+}
