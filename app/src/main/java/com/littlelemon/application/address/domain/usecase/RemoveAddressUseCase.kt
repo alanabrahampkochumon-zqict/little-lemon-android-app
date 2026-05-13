624e6a341a551dad6c35579963f8c5d0f2bcabeb
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-13 17:16:28 +0530

    feat: add remove address usecase

---
+++ app/src/main/java/com/littlelemon/application/address/domain/usecase/RemoveAddressUseCase.kt
@@ -0,0 +1,8 @@
+package com.littlelemon.application.address.domain.usecase
+
+import com.littlelemon.application.address.domain.AddressRepository
+import com.littlelemon.application.address.domain.models.LocalAddress
+
+class RemoveAddressUseCase(private val repository: AddressRepository) {
+    suspend fun invoke(address: LocalAddress) = repository.removeAddress(address)
+}
