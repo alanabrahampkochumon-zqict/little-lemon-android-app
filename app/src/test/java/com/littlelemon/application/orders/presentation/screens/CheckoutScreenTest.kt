c504eab0402650a5290b492670fbaef45abce825
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-14 16:30:27 +0530

    test: add test to verify callback logic of checkout screen

---
+++ app/src/test/java/com/littlelemon/application/orders/presentation/screens/CheckoutScreenTest.kt
@@ -0,0 +1,38 @@
+package com.littlelemon.application.orders.presentation.screens
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import com.littlelemon.application.R
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import kotlin.test.assertTrue
+
+
+@RunWith(RobolectricTestRunner::class)
+class CheckoutScreenTest {
+
+
+    @get:Rule
+    val testRule = createComposeRule()
+
+    private val application = RuntimeEnvironment.getApplication()
+
+    @Test
+    fun onClickBack_triggersCallback() = runTest {
+        var callbackTriggered = false
+        testRule.setContent {
+            CheckoutScreen({ callbackTriggered = true })
+        }
+
+        testRule.onNodeWithContentDescription(application.getString(R.string.back_to_cart))
+            .performClick()
+
+        assertTrue { callbackTriggered }
+    }
+
+}
