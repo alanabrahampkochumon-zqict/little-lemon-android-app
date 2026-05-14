c21aaa8de5d5cc35ad5a5d9d7ab30aa6db28bba8
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-14 16:27:08 +0530

    feat: add checkout screen header

--- app/src/main/java/com/littlelemon/application/orders/presentation/screens/CheckoutScreen.kt
+++ app/src/main/java/com/littlelemon/application/orders/presentation/screens/CheckoutScreen.kt
@@ -2,10 +2,18 @@ package com.littlelemon.application.orders.presentation.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -14,19 +22,26 @@ import com.littlelemon.application.R
 import com.littlelemon.application.core.presentation.designsystem.LittleLemonTheme
 
 @Composable
-fun CheckoutScreen(modifier: Modifier = Modifier) {
+fun CheckoutScreen(onNavigateBack: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
             .background(LittleLemonTheme.colors.primary)
     ) {
-        Row {
-            Image(
-                painterResource(R.drawable.ic_x),
-                contentDescription = stringResource(R.string.back_to_cart)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(modifier = Modifier.clickable(onClick = onNavigateBack).minimumInteractiveComponentSize()) {
+                Image(
+                    painterResource(R.drawable.ic_x),
+                    contentDescription = stringResource(R.string.back_to_cart)
+                )
+            }
+            Text(
+                stringResource(R.string.review_order),
+                style = LittleLemonTheme.typography.displaySmall
             )
-//            Text(stringResource(R.string.review_order))
         }
+        Spacer(Modifier.height(LittleLemonTheme.dimens.sizeMD))
+        HorizontalDivider(color = LittleLemonTheme.colors.outlineSecondary)
         // Order cards
         // Delivering to
         // Order Summary
@@ -38,6 +53,6 @@ fun CheckoutScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun CheckoutScreenPreview() {
     LittleLemonTheme {
-        CheckoutScreen()
+        CheckoutScreen({})
     }
 }
